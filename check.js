1f6dbc88cccfaf5d7ee3c957ef49727ee032a527
liujingjingg <949694044@qq.com>
2020-03-18 10:03:23 +0800

    add check

--- check.js
+++ check.js
@@ -2,7 +2,6 @@ $(function() {
     var $user = $('#user'),
         $telephone = $('#telephone'),
         $password = $('#password'),
-        $code = $('#code'),
         $acquire = $('#acquire'),
         $register = $('#register');
     // 得到焦点
@@ -12,61 +11,52 @@ $(function() {
     $password.focus(function(){
         $('#password-validation-message').html('长度为8~14个字符，字母/数字以及标点符号至少两种，不允许有中文、空格');            
       });
+
+    $register.click(function(){
+        if(!validate('#user') || !validate('#telephone') || !validate('#password') || !validate('#code') )return;
+        if(validate('#user') && validate('#telephone') && validate('#password') && validate('#code') ){
+          alert('注册成功!');
+        }
+        
+    })
     //用户名校验
-    var user = false;
     $user.focusout(function(){
       var msg = $('#user-validation-message');
-      if($user.val() === ''){
-        msg.html('不能为空!');           
-        return false;                 
-      }
       if(/[^\w\u4e00-\u9fa5]|^\d+$/.test($user.val())){
         msg.html('用户名仅支持中英文、数字和下划线，且不能为纯数字'); 
+        $user.select();
         return false;                       
       }
-      msg.html('');
-      user = true;
+      if(validate('#user')){
+        msg.html('')
+        return true;
+    }
     });
     //电话校验
-    var telephone = false;
     $telephone.focusout(function(){
       var msg = $('#telephone-validation-message');
-      if($telephone.val() === ''){
-        msg.html('不能为空!');
-        return false;       
-      }
       if(!/^1[3456789]\d{9}$/.test($telephone.val())){
         msg.html('手机号码格式不正确');
+        $telephone.select();
         return false;                  
       }
-      msg.html('');
-        telephone = true;
+      if(validate('#telephone')){
+        msg.html('')
+        return true;
+    }
     });   
     //密码
-    var password=false;
     $password.focusout(function(){
       var msg = $('#password-validation-message');
-      if($password.val() === ''){
-        msg.html('不能为空!');
-        return false;        
-      }
       if(!(/(?!^[0-9]+$)(?!^[A-z]+$)(?!^[^A-z0-9]+$)^[^\s\u4e00-\u9fa5]{8,14}$/).test($password.val())){
         msg.html('长度为8~14个字符，字母/数字以及标点符号至少两种，不允许有中文、空格');
+        $password.select();
         return false;                     
       }
-      msg.html('');
-      password = true;              
-    })
-    //验证码
-    var code=false;
-    $code.focusout(function(){
-      var msg = $('#code-validation-message');
-      if($code.val() === ''){
-        msg.html('不能为空!');
-        return false;      
-      }
-      msg.html('');
-      code = true;
+      if(validate('#password')){
+        msg.html('')
+        return true;
+    }            
     })
     //验证码倒计时
     var time,i=60;
@@ -83,12 +73,18 @@ $(function() {
             }
         },1000)
     })
-    //注册成功
-    $register.click(function(){
-      if(user&& password && telephone && code){
-        alert('注册成功!');
+    //表单级验证
+    function validate(field){
+      var $data=$(field),
+          msg=$(field+'-validation-message');
+      if($data.val()===''){
+        msg.html('不能为空');
+        $data.select();
+        return false;
       }
-    }) 
+      msg.html('');
+      return true;
+    }
   })
 
 
